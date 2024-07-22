223231bf6805f2a7e573633be587bcf4c37b915d
Jameson <jameson.stickle@itas.ca>
2024-07-22 14:21:24 -0700

    working on authentication, can add instructors using register page

--- my-attendance-app/src/firebaseConfig.js
+++ my-attendance-app/src/firebaseConfig.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -20,6 +19,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
-const analytics = getAnalytics(app);
 
-export { db, auth };
+export { firebaseConfig, db, auth };
